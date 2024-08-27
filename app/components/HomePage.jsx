335e9b5d9e42621005e767825b9b4936023a5979
Ankit Thapar <knowergenius1922@gmail.com>
2024-08-27 14:23:31 -0400

    Made changes to HomePage component

--- app/components/HomePage.jsx
+++ app/components/HomePage.jsx
@@ -1,32 +1,33 @@
 export default function HomePage() {
   return (
-    // Mobile
-    <div className="h-[92vh] flex items-center justify-center">
-      <div className="pl-8">
-        <p className="text-4xl md:text-5xl font-inter font-semibold inline">
-          Take Control of <br />
-          Your Finances <br />
-          with Broke Buddy
-        </p>
+    <>
+      <div className="h-[92vh] flex items-center justify-center">
+        <div className="pl-8">
+          <p className="text-4xl md:text-5xl font-inter font-semibold inline">
+            Take Control of <br />
+            Your Finances <br />
+            with Broke Buddy
+          </p>
 
-        <br />
-        <br />
-        <p className="text-2xl font-inter inline">
-          Easily track your expenses, manage your budget, and
           <br />
-          visualize your spendings
-        </p>
+          <br />
+          <p className="text-2xl font-inter inline">
+            Easily track your expenses, manage your budget, and
+            <br />
+            visualize your spendings
+          </p>
 
-        <br />
-        <br />
-        <button className="bg-black text-white px-4 py-2 rounded-lg mr-4">Get started</button>
+          <br />
+          <br />
+          <button className="bg-black text-white px-4 py-2 rounded-lg mr-4">Get started</button>
 
-        <button className="bg-[#ADB0B2] text-white px-4 py-2 rounded-lg">Learn More </button>
+          <button className="bg-[#ADB0B2] text-white px-4 py-2 rounded-lg">Learn More </button>
+        </div>
+        <img
+          src="/home_page_image.png"
+          className="w-1/2 h-auto absolute bottom-0 right-0 md:static md:w-1/4"
+        />
       </div>
-      <img
-        src="/home_page_image.png"
-        className="w-1/2 h-auto absolute bottom-0 right-0 md:static md:w-1/4"
-      />
-    </div>
+    </>
   );
 }
