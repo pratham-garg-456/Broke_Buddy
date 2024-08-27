5a4319fe60af357b87928052ee63430c6e2bfba6
Ankit Thapar <knowergenius1922@gmail.com>
2024-08-27 14:34:03 -0400

    Corrected route for HomePage component

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Navbar from '@/app/components/navbar';
 import Footer from '@/app/components/footer';
 import Layout from '@/app/layout';
-import HomePage from '@/app/HomePage';
+import HomePage from '@/app/components/HomePage';
 export default function Home() {
   return <Layout header={<Navbar />} main={<HomePage />} footer={<Footer />}></Layout>;
 }
