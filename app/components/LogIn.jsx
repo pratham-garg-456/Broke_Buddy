4a2bd0dcca4f5729d86d8608dfbaa3f85c3e4c04
Ankit Thapar <knowergenius1922@gmail.com>
2024-09-08 21:44:05 -0400

    Fixed errors Log In

--- app/components/LogIn.jsx
+++ app/components/LogIn.jsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+import google from '../../public/google.png';
 export default function LogIn() {
   return (
     <div className="flex justify-center mt-28 mb-20 font-inter">
-      <div className="border border-black border-[#BEBFBF] md:w-96">
+      <div className="border border-[#BEBFBF] md:w-96">
         <div className="p-10">
           <div className="mb-10">
             <p className="font-jockeyOne text-3xl text-center">BrokeBuddy</p>
@@ -9,29 +11,31 @@ export default function LogIn() {
           <div className="mb-3">
             <input
               placeholder="Phone number, username, or email"
-              className="border rounded-md border [#BEBFBF] p-2 w-full text-sm"
+              className="border rounded-md border [#BEBFBF] p-2 w-full text-xs md:text-sm"
             />
           </div>
           <div className="mb-3">
             <input
               placeholder="Password"
               type="Password"
-              className="border rounded-md border [#BEBFBF] p-2 w-full text-sm"
+              className="border rounded-md border [#BEBFBF] p-2 w-full text-xs md:text-sm"
             />
           </div>
           <div className="flex justify-center mb-6">
-            <button type="submit" className="bg-black text-white rounded-md text-lg px-4 py-2">
+            <button type="submit" className="bg-black text-white rounded-md md:text-lg px-4 py-2">
               Login
             </button>
           </div>
           <div className="flex justify-center items-center mb-4">
             <hr className="border-1 w-[40%] border-black border-[#B4B5B5] inline-block" />
-            <p className="inline-block w-[20%] text-center font-inter text-[#404040] text-md">OR</p>
+            <p className="inline-block w-[20%] text-center font-inter text-[#404040] text-sm md:text-md">
+              OR
+            </p>
             <hr className="border-1 w-[40%] border-black border-[#B4B5B5] inline-block" />
           </div>
           <div className="flex justify-center mb-7">
             <div className="mr-1">
-              <img src="/google.png" className="w-6" />
+              <Image className="w-6 object-contain" src={google} alt="<Member Photo>"></Image>
             </div>
             <div>
               <button className="text-[#404040] text-sm underline font-inter">
@@ -40,7 +44,7 @@ export default function LogIn() {
             </div>
           </div>
           <div className="flex justify-center mb-7">
-            <p className="mr-1">Don't have an account? </p>
+            <p className="mr-1">Don&apos;t have an account? </p>
             <button className="font-bold text-sm">Sign Up</button>
           </div>
         </div>
